0a87c27c06b877d1047d2e30a09fc6af2e427f02
BogdanFloris <bogdan.floris@gmail.com>
2025-04-09 08:42:35 +0300

    feat: add home manager

--- flake.nix
+++ flake.nix
@@ -4,16 +4,28 @@
   inputs = {
     # Pin NixOS packages to a specific stable release
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { self, nixpkgs, ... }@inputs:
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }@inputs:
     {
       # Define a NixOS configuration named 'nixos-vm'
       nixosConfigurations.nixos-vm = nixpkgs.lib.nixosSystem {
         system = "aarch64-linux"; # M Mac's VMWare Fusion VM
         specialArgs = { inherit inputs; }; # Pass flake inputs to modules
         modules = [
+          home-manager.nixosModules.home-manager
+
           # Load the main system configuration file
           ./configuration.nix
           ./hardware-configuration.nix
