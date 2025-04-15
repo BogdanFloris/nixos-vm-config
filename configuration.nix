445bb7324cf8b01e3aa9f88919fd1325c1293e4f
Bogdan Floris <bogdan.floris@gmail.com>
2025-04-15 13:39:49 +0300

    feat: remove custom vmware settings

--- configuration.nix
+++ configuration.nix
@@ -91,38 +91,6 @@
     open-vm-tools
   ];
 
-  systemd.services.vmware = {
-    description = "VMWare Guest Service";
-    wantedBy = [ "multi-user.target" ];
-    after = [ "display-manager.service" ];
-    unitConfig.ConditionVirtualization = "vmware";
-    serviceConfig.ExecStart = "${pkgs.open-vm-tools}/bin/vmtoolsd";
-  };
-
-  # Mount the vmblock for drag-and-drop and copy-and-paste.
-  systemd.mounts = [{
-    description = "VMware vmblock fuse mount";
-    documentation = [
-      "https://github.com/vmware/open-vm-tools/blob/master/open-vm-tools/vmblock-fuse/design.txt"
-    ];
-    unitConfig.ConditionVirtualization = "vmware";
-    what = "${pkgs.open-vm-tools}/bin/vmware-vmblock-fuse";
-    where = "/run/vmblock-fuse";
-    type = "fuse";
-    options = "subtype=vmware-vmblock,default_permissions,allow_other";
-    wantedBy = [ "multi-user.target" ];
-  }];
-
-  security.wrappers.vmware-user-suid-wrapper = {
-    setuid = true;
-    owner = "root";
-    group = "root";
-    source = "${pkgs.open-vm-tools}/bin/vmware-user-suid-wrapper";
-  };
-
-  environment.etc.vmware-tools.source =
-    "${pkgs.open-vm-tools}/etc/vmware-tools/*";
-
   virtualisation.vmware.guest.enable = true;
 
   # Share the host's filesystem with the VM
