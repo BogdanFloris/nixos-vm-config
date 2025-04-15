5069ba664fab09f8a004742f0d97616b0d53533a
Bogdan Floris <bogdan.floris@gmail.com>
2025-04-15 13:26:54 +0300

    feat: autoscaling

--- configuration.nix
+++ configuration.nix
@@ -81,6 +81,11 @@
     killall
     niv
     man-pages
+    # For hypervisors that support auto-resizing, this script forces it.
+    # I've noticed not everyone listens to the udev events so this is a hack.
+    (writeShellScriptBin "xrandr-auto" ''
+      xrandr --output Virtual-1 --auto
+    '')
 
     # VMware Guest Additions
     open-vm-tools
