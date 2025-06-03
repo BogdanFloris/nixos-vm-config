88a2ee4d7539e10cfb7f3b0cb3954cbd3cd93d58
Bogdan Floris <bogdan.floris@gmail.com>
2025-06-03 14:52:42 +0300

    feat: add dropbar

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -403,6 +403,21 @@ require("lazy").setup({
 		opts = {},
 	},
 
+	{
+		"Bekaboo/dropbar.nvim",
+		-- optional, but required for fuzzy finder support
+		dependencies = {
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+		},
+		config = function()
+			local dropbar_api = require("dropbar.api")
+			vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
+			vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
+			vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
+		end,
+	},
+
 	require("plugins.blink"),
 	require("plugins.none_ls"),
 	require("plugins.harpoon"),
